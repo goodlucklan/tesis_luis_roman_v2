83e456d6f8def1caa1cc9d955f282763514c33af
Diego <diegourbina1284@gmail.com>
2023-12-09 00:28:41 -0500

    Se avanzó con estructura del módulo entradas y salidas

--- src/pages/private/inputs-and-outputs.jsx
+++ src/pages/private/inputs-and-outputs.jsx
@@ -1,3 +1,24 @@
+import { Stack, Container, Typography } from '@mui/material';
+
+import { DataTable } from 'src/components/table';
+
 export default function InputsAndOutputsPage() {
-  return <h1>InputsAndOutputs</h1>;
+  const headers = [
+    { id: 'product', label: 'Producto' },
+    { id: 'movementType', label: 'Tipo de Movimiento', align: 'center' },
+    { id: 'quantity', label: 'Cantidad', align: 'center' },
+    { id: 'category', label: 'Categoría', align: 'center' },
+    { id: 'date', label: 'Fecha', align: 'center' },
+    { id: 'location', label: 'Locación', align: 'center' },
+    { id: '' },
+  ];
+
+  return (
+    <Container>
+      <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
+        <Typography variant="h4">Entradas y Salidas</Typography>
+      </Stack>
+      <DataTable headers={headers} items={[]} />
+    </Container>
+  );
 }
