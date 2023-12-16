6b0f3569ff3cae82be5bfbfa87e5abe926b2b44b
ZeroxD Roman Pardo <lromanp@utp.edu.pe>
2023-12-15 19:30:10 -0500

    develop

---
+++ src/hooks/use-kardex-data.js
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react';
+import { query, where, getDocs, collection } from 'firebase/firestore';
+
+import { db } from '../firebase/firebase';
+
+const movimientos = await query(
+  collection(db, 'Movimiento'),
+  where('aprobacion', '==', 'Aprobado')
+);
+
+const useMovimientoApprove = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const obtenerMovimientoconCostoAprobado = async () => {
+      const movimientoSnapShot = await getDocs(movimientos);
+      movimientoSnapShot.docs.map(async (movDoc) => {
+        const movementData = movDoc.data();
+        const productoNombre = movementData.producto;
+        const Products = await query(
+          collection(db, 'Productos'),
+          where('nombre', '==', productoNombre)
+        );
+        const productSnapShot = await getDocs(Products);
+        if (!productSnapShot.empty) {
+          const productoData = productSnapShot.docs[0].data();
+          const { costo } = productoData;
+          const result = [];
+          result.push({
+            id: movDoc.id,
+            product: movementData.producto,
+            movementType: movementData.Tipo_Movimiento,
+            quantity: movementData.cantidad,
+            costo,
+            reason: movementData.motivo,
+            category: movementData.categoria,
+            location: movementData.locacion,
+            date: movementData.fecha,
+            value: parseInt(movementData.cantidad, 10) * costo,
+          });
+          setData(result);
+        }
+      });
+    };
+    obtenerMovimientoconCostoAprobado();
+  }, []);
+  return { data };
+};
+export default useMovimientoApprove;
--- src/pages/private/report.jsx
+++ src/pages/private/report.jsx
@@ -1,17 +1,26 @@
+import { useMemo } from 'react';
+
 import { Stack, Container, Typography } from '@mui/material';
 
-import { movements } from 'src/_mock/movements';
+import useMovimientoApprove from 'src/hooks/use-kardex-data';
+
+// import { movements } from 'src/_mock/movements';
 
 import { DataTable } from 'src/components/table';
 
 export default function ReportPage() {
+  const { data } = useMovimientoApprove();
+  const kardex = useMemo(() => data, [data]);
   const headers = [
     { id: 'product', label: 'Producto' },
     { id: 'movementType', label: 'Tipo de Movimiento', align: 'center' },
     { id: 'quantity', label: 'Cantidad', align: 'center' },
+    { id: 'costo', label: 'Costo', align: 'center' },
     { id: 'reason', label: 'Motivo', align: 'center' },
     { id: 'category', label: 'Categoría', align: 'center' },
     { id: 'location', label: 'Locación', align: 'center' },
+    { id: 'date', label: 'fecha', align: 'center' },
+    { id: 'value', label: 'Valor total', align: 'center' },
   ];
 
   return (
@@ -19,7 +28,7 @@ export default function ReportPage() {
       <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
         <Typography variant="h4">Kardex</Typography>
       </Stack>
-      <DataTable searchParameter="product" headers={headers} items={movements} />
+      <DataTable searchParameter="product" headers={headers} items={kardex} />
     </Container>
   );
 }
